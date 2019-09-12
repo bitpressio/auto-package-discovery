2853e2dd077fda7c1560d60d12b72acb542a4a51
Gummibeer <dev.gummibeer@gmail.com>
2019-09-12 14:10:34 +0200

    remove unused import

--- src/DiscoverCommand.php
+++ src/DiscoverCommand.php
@@ -6,7 +6,6 @@ use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
