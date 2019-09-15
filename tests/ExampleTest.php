4ec5085d7af109e30ed7d195c54fd4096a9a5186
Paul Redmond <paulrredmond@gmail.com>
2019-09-14 17:26:27 -0700

    Adding PHPUnit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BitPress\AutoDiscovery\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BitPress\AutoDiscovery\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+}
